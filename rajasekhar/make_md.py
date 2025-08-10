c7fd4083910bb8bb88a950c3476b08cfbbe9b71e
rsb-23 <57601627+rsb-23@users.noreply.github.com>
2025-08-10 16:14:05 +0530

    added video link

--- rajasekhar/make_md.py
+++ rajasekhar/make_md.py
@@ -1,18 +1,28 @@
 import yt_description
 from common import get_video_data, save_locally
 
+STORY_FORMAT = """
+### [{title}](https://youtu.be/{video_id})
 
-def make_story(title, story) -> str:
+<details>
+<summary>Story</summary>
+{story}
+</details>
+"""
+
+
+def make_story(video_data: dict) -> str:
+    story = video_data["description"]
     if not story:
         return ""
     _story = "<br><br>".join(x for x in story if len(x) > 100)
-    return f"\n### {title}\n\n<details>\n<summary>Story</summary>\n{_story}\n</details>\n"
+    return STORY_FORMAT.format(video_id=video_data["id"], title=video_data["title"], story=_story)
 
 
 def index_md_content() -> str:
     index_md = "## Raja Mama Stories\n"
     for video_detail in VIDEO_DATA.values():
-        index_md += make_story(video_detail["title"], video_detail["description"])
+        index_md += make_story(video_detail)
     return index_md
 
 
